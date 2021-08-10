b2d489ded29101483e7d96e82a1d943b4616c530
k3cman <kecman.dev@gmail.com>
2021-08-10 16:52:58 +0200

    cleanup comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,21 +9,15 @@ module.exports = {
     "plugin:prettier/recommended",
   ],
   rules: {
-    // Turns of Airbnb React prefer default export
     "import/no-unresolved": "off",
     "import/prefer-default-export": "off",
     "class-methods-use-this": "off",
-    // If we turn this on, any regex we are using will be read as an error
     "no-useless-escape": "warn",
-    // We need this turned off because we are using _ for BehaviourSubject naming in services
     "no-underscore-dangle": "off",
-    // Stupid rule for React
     "@typescript-eslint/lines-between-class-members": "off",
-    // We can use this in the future but right now we are using + to concatenate strings in most of the places
     "prefer-template": "off",
     // No param reassign should be enabled in order to enforce immutable code style
     "no-param-reassign": "off",
-    // We need this for new coding style where we use only if and not else and we use early return if the conditions are not met (by dusan's recommendation)
     "no-useless-return": "off",
     // THis disabled using of prototype calls like hasOwnProperty
     // TODO: Discuss should we turn this on but then we will need to use in or hasOwnPropety.call(obj,'property')
